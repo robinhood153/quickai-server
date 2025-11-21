275d161cf0733acba8ab8ceed5ac5b48fa8c3745
robinhood153 <adityabinorkar@gmail.com>
2025-11-21 22:55:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
 
-async function startServer() {
-    const app = express()
 
-    // Cloudinary connection
-    await connectCloudinary()
+const app = express()
 
-    app.use(cors({
-        origin: "*",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type", "Authorization"]
-    }))
+await connectCloudinary()
 
-    app.use(express.json())
-    app.use(clerkMiddleware())
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
 
-    app.get('/', (req, res) => res.send('Server is Live!'))
+// dotenv.config()
 
-    app.use('/api/ai', aiRouter)
-    app.use('/api/user', userRouter)
+app.get('/', (req, res)=>res.send('Server is Live!'))
 
-    const PORT = process.env.PORT || 3000
+app.use(requireAuth())
 
-    app.listen(PORT, () =>
-        console.log(`Server running on port ${PORT}`)
-    )
-}
+app.use('/api/ai', aiRouter)
+app.use('/api/user', userRouter)
 
-startServer()
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('Server is running in port', PORT);
+    
+})

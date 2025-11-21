5afe2bf1a450aa057733a7670e5d70db7c55a5e9
robinhood153 <adityabinorkar@gmail.com>
2025-11-21 22:37:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import { clerkMiddleware, requireAuth } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
 
+async function startServer() {
+    const app = express()
 
-const app = express()
+    // Cloudinary connection
+    await connectCloudinary()
 
-await connectCloudinary()
+    app.use(cors({
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }))
 
-app.use(cors())
-app.use(express.json())
-app.use(clerkMiddleware())
+    app.use(express.json())
+    app.use(clerkMiddleware())
 
-// dotenv.config()
+    app.get('/', (req, res) => res.send('Server is Live!'))
 
-app.get('/', (req, res)=>res.send('Server is Live!'))
+    app.use('/api/ai', aiRouter)
+    app.use('/api/user', userRouter)
 
+    const PORT = process.env.PORT || 3000
 
-app.use('/api/ai', aiRouter)
-app.use('/api/user', userRouter)
+    app.listen(PORT, () =>
+        console.log(`Server running on port ${PORT}`)
+    )
+}
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()=>{
-    console.log('Server is running in port', PORT);
-    
-})
+startServer()
